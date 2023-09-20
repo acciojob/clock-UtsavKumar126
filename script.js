019c3ff09aa1b68d7878d443927884c4f22ec8d2
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-20 13:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function updateDateTime() {
+            const dateTimeElement = document.getElementById("timer");
+            const now = new Date();
+            const dateTimeString = now.toLocaleString(); // Format the date and time as a string
+            dateTimeElement.textContent = dateTimeString;
+        }
+
+        // Initial call to display the date and time
+        updateDateTime();
+
+        // Update the date and time every second (1000 milliseconds)
+        setInterval(updateDateTime, 1000);
